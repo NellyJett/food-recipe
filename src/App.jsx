7ab33eac8f1043a07f4f7c65f1d3199524e643e5
Nelly-B <alapanelly@gmail.com>
2024-07-10 18:17:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+import Outer from '../../components/Outer'
+
+
+function App() {
+  return (
+    <>
+    <div>
+    <Outer></Outer>
+    </div>
+      
+      
+    </>
+  )
+}
+
+export default App
